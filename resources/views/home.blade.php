11e3f6116c32613c9e0f4ef9e98509424489ee9d
Gabriela Casillas <csgaby7@gmail.com>
2017-06-04 11:03:03 -0500

    cambio diseño y nuevas funcionalidades de auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,10 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">LISTADO DE POSTS</div>
+                <div class="panel-heading">Dashboard</div>
+
                 <div class="panel-body">
-					<ul>
-						@forelse ($posts as $post)
-							<li>{{ $post->content }}</li>
-						@empty
-							<p>No tiene ningun post </p>
-						@endforelse
-					</ul>
+                    You are logged in!
                 </div>
             </div>
         </div>
--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,17 +1,30 @@
-@extends('layout')
+
+
+@extends('layouts.app')
 
 @section('content')
-	<div class="media">
-		<div class="media-left">
-			<<a href="#" title="">
-				<img src=". . ." alt="..." class="media-object">
-			</a>
-		</div>
-		<div class="media-body">
-	        {{ $post->content }}
-	        <footer>
-	              Creado {{ $post->created_at }}
-	        </footer>
-	     </div>
-	</div>
-@stop
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">POST</div>
+                <div class="panel-body">
+                	<div class="media">
+						<div class="media-left">
+							<a href="#" title="">
+								<img src=". . ." alt="..." class="media-object">
+							</a>
+						</div>
+						<div class="media-body">
+					        {{ $post->content }}
+					        <footer>
+					              Creado {{ $post->created_at }}
+					        </footer>
+					     </div>
+					</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
